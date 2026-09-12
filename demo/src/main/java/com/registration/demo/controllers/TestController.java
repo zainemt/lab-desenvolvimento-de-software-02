9a3663681722207da14f348092de7c5f8a3072a4
Zaine Mendes <zainemendes@hotmail.com>
2026-09-12 12:30:16 -0300

    initial commmit - project setup

---
+++ demo/src/main/java/com/registration/demo/controllers/TestController.java
@@ -0,0 +1,17 @@
+package com.registration.demo.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("Hello, World!");
+    }
+
+}
